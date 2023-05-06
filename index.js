ce607fc5a22d40690d9a367b4455e3b443b5e67e
sillajaxon <sillajaxon@gmail.com>
2023-05-06 13:07:36 +0300

    home endpoints

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+
+const home = require("./routes/index");
+const apihome = require("./routes/index");
 const product = require("./routes/product");
 
 app.use(express.json({ extended: false }));
 
+app.use("/", home);
+app.use("/api", apihome);
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
