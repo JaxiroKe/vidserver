659a14eb9a608006e0029b05aa7215f6a4f16c77
sillajaxon <sillajaxon@gmail.com>
2023-05-06 12:39:57 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-const home = require("./routes/index");
 const product = require("./routes/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/", home);
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 300;
