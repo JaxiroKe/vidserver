1fa2f8a4fec95365973b9799e8ee352437657a3c
sillajaxon <sillajaxon@gmail.com>
2023-05-06 12:13:03 +0300

    another try to deploy to vercel

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use("/api/product", product);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Vidshop server listening on port ${port}`)
   })
   
